a31d39bf442c9a55121d1a9fb523793a840722b2
Thibault Dethier <tdethier@bluesquarehub.com>
2024-05-07 11:40:43 +0200

    Fixed installation script

--- scripts/setup_local_with_main_openimis_fe_app.sh
+++ scripts/setup_local_with_main_openimis_fe_app.sh
@@ -2,14 +2,14 @@
 
 libs='react react-dom react-router react-router-dom "@material-ui/core" react-redux'
 (
-  cd ../openimis-dev/openimis-fe_js/node_modules/
+  cd ../openimis-fe_js/node_modules/
   for lib in $libs; do
     (cd $lib && yarn link)
   done
 )
 yarn install
 for lib in $libs; do
-  yarn $lib
+  yarn link $lib
 done
 yarn link
-(cd ../openimis-dev/openimis-fe_js/ && yarn link "@openimis/fe-qmoney-payment")
+(cd ../openimis-fe_js/ && yarn link "@openimis/fe-qmoney-payment")
